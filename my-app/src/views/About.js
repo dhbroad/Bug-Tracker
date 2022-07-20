1e276ffc7b3447b1cb1cab52a2771a80343c267b
dhbroad <dhbroad@udel.edu>
2022-07-19 20:55:37 -0400

    about

---
+++ my-app/src/views/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+          <h4>This bug tracker was developed by David Broadwater for keeping track of errors during the coding process</h4>
+          <h4>Multiple users will be able to track progress on one project.</h4>
+      </div>
+    )
+  }
+}
